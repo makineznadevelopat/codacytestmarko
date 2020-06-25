928512cc356e8997c5b8bb36b85e7aaa9f0d63ce
Marko Babic <marko.babic@moje-aplikacije.net>
2020-06-25 21:59:47 +0200

    update

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const obj = {
   test: 1,
   second: 3,
-  3: '3'
-}
+  3: "3"
+};
 
 const stack = []
 
 Object.keys(obj).forEach(function (property) {
 // eslint-disable-next-line security/detect-object-injection
-  stack.push(obj[property])
-})
-
-console.log(stack)
+  stack.push(obj[property]);
+});
